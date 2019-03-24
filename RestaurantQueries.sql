50ea682361b33a02270a017c167319553eda087e
itszp <tayzp@u.nus.edu>
2019-03-24 21:39:08 +0800

    Added seats

--- RestaurantQueries.sql
+++ RestaurantQueries.sql
@@ -2,3 +2,13 @@
 select rname
 from Restaurants natural join Outlets
 where area = 'Central';
+
+-- Display restaurant and their average ratings
+select R.rname, avg(ratingscore) as averageScore
+from Restaurants R natural join Ratings RT
+group by R.rname
+
+-- Display restaurant:HaiDiLao's reviews
+select R.rname, RT.review
+from Restaurants R natural join Ratings RT
+where R.rname = 'HaiDiLao';
