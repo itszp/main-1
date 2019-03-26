87621262825a161585d57d67cfebb8de15e79b82
itszp <tayzp@u.nus.edu>
2019-03-26 17:01:53 +0800

    added all data and innput into postgre.sql;

---
+++ sql/ZackWork/addPoints.sql
@@ -0,0 +1,5 @@
+delete from Points;
+
+INSERT INTO Points (pointNumber, rsvid, userid) VALUES
+(50, 1, 1),
+(20, 2, 2);
--- sql/index.js
+++ sql/index.js
@@ -5,6 +5,7 @@ const sql = {};
 sql.query = {
 	add_user: 'INSERT INTO users (userid, username, userpassword, fullname, phoneno) VALUES ($1,$2,$3,$4, $5)',
 	userpass: 'SELECT * FROM users WHERE username = $1'
+	
 }
 
 module.exports = sql
